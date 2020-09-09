51bf47e3395f73c6125551b24619ac6d822414c5
Hojjat <h.galeh@gmail.com>
2020-09-09 10:40:49 +0430

    simple app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { BlogPostTileComponent } from './blog-post-tile/blog-post-tile.component';
-import { BlogListComponent } from './blog-list/blog-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BlogPostTileComponent,
-    BlogListComponent
   ],
   imports: [
     BrowserModule
